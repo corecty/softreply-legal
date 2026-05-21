848406f750719901c80971bc17e38862958e860d
Bergucci <bergucciweb@gmail.com>
2026-05-22 08:46:19 +0900

    Add SoftReply legal files

---
+++ apps/mobile/app/legal/privacy.tsx
@@ -0,0 +1,5 @@
+import { LegalDocumentScreen } from '@/features/legal/LegalDocumentScreen';
+
+export default function PrivacyScreen() {
+  return <LegalDocumentScreen documentKey="privacy" />;
+}
---
+++ apps/mobile/app/legal/terms.tsx
@@ -0,0 +1,5 @@
+import { LegalDocumentScreen } from '@/features/legal/LegalDocumentScreen';
+
+export default function TermsScreen() {
+  return <LegalDocumentScreen documentKey="terms" />;
+}
